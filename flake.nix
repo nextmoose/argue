d7fb47f750e3411709985e5b5e92e2ddf6b2df50
Emory Merryman <emory.merryman@gmail.com>
2022-11-20 15:46:01 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -36,7 +36,8 @@
 				  if ! success then [ builtins.elemAt test 3 ]
 				  else if ! eval.success then [ builtins.elemAt test 3 ]
 				  else [ ] ;
-				in { success = success ; value = value ; } ;
+				k = if eval.success then "Y" else "N" ;
+				in builtins.trace k { success = success ; value = value ; } ;
 			in test input-tests mapper "2993696e-6e89-41b8-a61a-98ce44b944c4" ;
 		  output =
 		    if ! input-test-results.success then input-test-results
