48663691fdbd0695f8bbbca5ad4c7a4c12567ac6
Emory Merryman <emory.merryman@gmail.com>
2022-11-20 13:43:12 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -63,13 +63,12 @@
 		    else builtins.tryEval ( to-string output.value ) ;
 	          test =
 		    results : mapper : error :
-		      builtins.trace ( builtins.concatStringsSep "," ( builtins.map builtins.typeOf [ results mapper error ] ) ) (
 		      if builtins.typeOf results != "list" then { success = false ; value = [ error ] ; }
 		      else
 		        let
 			  success = builtins.all ( test : test.success ) ( builtins.map mapper results ) ;
 			  value = builtins.concatLists ( builtins.map ( test : test.value ) ( builtins.map mapper results ) ) ;
-			  in { success = success ; value = value ; } ) ;
+			  in { success = success ; value = value ; } ;
 		  ticket = if builtins.typeOf label == "string" then label else "648c4ec2-8287-455e-8bc1-4b2de45b0b4e" ;
 		  in
 		    {
